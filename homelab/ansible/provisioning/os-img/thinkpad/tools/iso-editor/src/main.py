9e8aeed359b7f202ef00321cc0ddc18e3c53d192
Emilia <voxelost@proton.me>
2023-10-02 19:40:49 +0200

    add an iso preseeding tool

---
+++ homelab/ansible/provisioning/os-img/thinkpad/tools/iso-editor/src/main.py
@@ -0,0 +1,49 @@
+import os
+import logging
+from utils import Multipass
+
+
+ISO_FILENAME = "debian-12.1.0-amd64-netinst.iso"
+
+if __name__ == "__main__":
+    logging.getLogger().setLevel(os.getenv("LOG_LEVEL", "INFO"))
+
+    with Multipass() as multipass:
+        multipass.cmd("apt-get update")
+        multipass.cmd("apt-get upgrade -y")
+        multipass.cmd("apt-get -y install genisoimage xorriso isolinux udevil")
+
+        if os.path.isfile(f".cache/{ISO_FILENAME}"):
+            logging.info("Found a cached .iso file, uploading to builder")
+            multipass.upload(f".cache/{ISO_FILENAME}")
+        else:
+            multipass.cmd(
+                f"wget http://ftp.icm.edu.pl/pub/Linux/debian-cd/12.1.0/amd64/iso-cd/{ISO_FILENAME}"
+            )
+            multipass.download(ISO_FILENAME, f".cache/{ISO_FILENAME}")
+        multipass.cmd(f"udevil mount {ISO_FILENAME} /media/root/{ISO_FILENAME}")
+        multipass.cmd(f"cp -rT /media/root/{ISO_FILENAME} isofiles/")
+        multipass.cmd("chmod +w -R isofiles/install.amd/")
+        multipass.cmd("gunzip isofiles/install.amd/initrd.gz")
+        multipass.upload(
+            "/Users/voxelost/workspace/devops/infra/homelab/ansible/provisioning/os-img/thinkpad/tools/iso-editor/src/preseed.cfg",
+            "preseed.cfg",
+        )
+        multipass.cmd(
+            "cpio -H newc -o -A -F isofiles/install.amd/initrd", stdin="preseed.cfg"
+        )
+        multipass.cmd("gzip isofiles/install.amd/initrd")
+        multipass.cmd("chmod -w -R isofiles/install.amd/")
+        multipass.cmd("chmod +w md5sum.txt", cwd="isofiles")
+        multipass.cmd(
+            "find -follow -type f ! -name md5sum.txt -print0 | xargs -0 md5sum > md5sum.txt",
+            cwd="isofiles",
+        )
+        multipass.cmd("chmod -w md5sum.txt", cwd="isofiles")
+        multipass.cmd(
+            f"xorriso -as mkisofs -o preseeded-{ISO_FILENAME} -isohybrid-mbr /usr/lib/ISOLINUX/isohdpfx.bin -c isolinux/boot.cat -b isolinux/isolinux.bin -no-emul-boot -boot-load-size 4 -boot-info-table isofiles",
+        )
+        multipass.cmd("chmod +w -R isofiles")
+        multipass.cmd("rm -r isofiles")
+        multipass.cmd(f"udevil unmount /media/root/{ISO_FILENAME}")
+        multipass.download(f"preseeded-{ISO_FILENAME}")
---
+++ homelab/ansible/provisioning/os-img/thinkpad/tools/iso-editor/src/utils.py
@@ -0,0 +1,117 @@
+import subprocess
+import logging
+import typing
+import uuid
+import os
+
+
+class MultipassException(Exception):
+    ...
+
+
+class Multipass:
+    _workdir = "/home/ubuntu/downloads"
+
+    def __init__(self, cpus: int = 4, memory: str = "8G"):
+        self._authenticate()
+
+        self._machine_name = f"geniso-{uuid.uuid4()}"
+        self._provision_vm(cpus, memory)
+
+        self.cmd(f"mkdir -p {self._workdir}", become=False, cwd=None)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, reason: typing.Optional[Exception], traceback, *args):
+        self._destroy_vm()
+
+    def _provision_vm(self, cpus: int = 4, memory: str = "8G") -> None:
+        logging.debug(f"Provisioning VM {self._machine_name}")
+        self._shell_cmd(
+            f"multipass launch --name {self._machine_name} --cpus {cpus} --memory {memory}"
+        )
+
+    def _destroy_vm(self) -> None:
+        logging.debug(f"Destroying VM {self._machine_name}")
+        self._shell_cmd(f"multipass delete {self._machine_name}")
+
+    def _authenticate(self):
+        logging.info(
+            "Authenticating multipass. You may need to input the sudo password now"
+        )
+        self._shell_cmd(f"sudo multipass authenticate {os.getenv('MULTIPASS_AUTH')}")
+
+    def _build_command(
+        self,
+        command: str,
+        become: bool = True,
+        cwd: typing.Optional[str] = _workdir,
+    ) -> str:
+        _cmd = command
+        if "|" in _cmd:
+            # handle commands with pipe
+            _cmd = f"'{_cmd}'"
+
+        if become:
+            _cmd = f"sudo {_cmd}"
+
+        _workdir = ""
+        if cwd is not None:
+            if not cwd.startswith("/"):
+                cwd = f"{self._workdir}/{cwd}"
+
+            _workdir = f"--working-directory {cwd}"
+
+        return f"multipass exec {_workdir} {self._machine_name} -- {_cmd}"
+
+    def _shell_cmd(self, cmd: str, stdin: str = "") -> str:
+        proc = subprocess.Popen(
+            cmd,
+            shell=True,
+            text=True,
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+
+        stdout, stderr = proc.communicate(stdin)
+
+        if proc.returncode != 0:
+            raise MultipassException(stderr)
+
+        return stdout.strip()
+
+    def _send_command(
+        self,
+        command: str,
+        become: bool = True,
+        cwd: typing.Optional[str] = _workdir,
+        stdin: str = "",
+    ) -> str:
+        logging.debug(f"Running command: {command}")
+        return self._shell_cmd(self._build_command(command, become, cwd), stdin)
+
+    def _transfer(self, src: str, dest: str):
+        return self._shell_cmd(f"sudo multipass transfer {src} {dest}")
+
+    def upload(self, src: str, dest: str = "."):
+        if not dest.startswith("/"):
+            dest = f"{self._workdir}/{dest}"
+
+        return self._transfer(src, f"{self._machine_name}:{dest}")
+
+    def download(self, src: str, dest: str = "."):
+        if not src.startswith("/"):
+            src = f"{self._workdir}/{src}"
+
+        return self._transfer(f"{self._machine_name}:{src}", dest)
+
+    def cmd(
+        self,
+        command: str,
+        become: bool = True,
+        cwd: typing.Optional[str] = _workdir,
+        stdin: str = "",
+    ) -> str:
+        return self._send_command(command, become, cwd, stdin)
