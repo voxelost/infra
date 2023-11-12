a4cd13fe61dd112db4f705a880c95d9e9b9e71fa
Emilia <voxelost@proton.me>
2023-11-12 19:49:24 +0100

    add draft rpi-imager wrapper

--- homelab/provisioning/tools/os-img/debian/src/builders/raspbian.py
+++ homelab/provisioning/tools/os-img/debian/src/builders/raspbian.py
@@ -11,8 +11,28 @@ def build_iso(
     target_iso_filename: str,
     multipass_auth: str,
 ):
-    with Multipass(target_config_file, multipass_auth, disk="20G") as multipass:
+    with Multipass(target_config_file, multipass_auth, disk="10GB") as multipass:
         # TODO: implement this logic:
         # https://github.com/raspberrypi/rpi-imager/blob/qml/src/OptionsPopup.qml#L589
 
-        logging.debug(multipass)
+        multipass.cmd("apt-get update")
+        multipass.cmd("apt-get upgrade -y")
+        multipass.cmd(
+            "apt-get install --no-install-recommends -y build-essential devscripts debhelper cmake git libarchive-dev libcurl4-gnutls-dev liblzma-dev qtbase5-dev qtbase5-dev-tools qtdeclarative5-dev libqt5svg5-dev qttools5-dev libgnutls28-dev qml-module-qtquick2 qml-module-qtquick-controls2 qml-module-qtquick-layouts qml-module-qtquick-templates2 qml-module-qtquick-window2 qml-module-qtgraphicaleffects e2fsprogs unp"
+        )
+        # https://downloads.raspberrypi.com/raspios_lite_arm64/images/raspios_lite_arm64-2023-10-10/2023-10-10-raspios-bookworm-arm64-lite.img.xz
+
+        multipass.cmd("git clone --depth 1 https://github.com/raspberrypi/rpi-imager")
+        multipass.cmd("debuild -uc -us", cwd="rpi-imager")
+        multipass.cmd("dpkg -i ./rpi-imager*.deb")
+        multipass.cmd("apt-get install -f")
+
+        multipass.upload(
+            "/Users/voxelost/workspace/devops/infra/homelab/provisioning/tools/os-img/debian/src/templates/first-run-raw.sh"
+        )
+
+        multipass.cmd("mkdir -p /dev/mock1")
+        multipass.cmd("mkdir -p mock1")
+        multipass.cmd("mount --bind /dev/mock1 mock1")
+
+        multipass.cmd("rpi-imager --cli --first-run-script first-run-raw.sh")
