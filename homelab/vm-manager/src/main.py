c8e090cf0df80cd79be5fa2deae6e4380cb81f5b
Emilia <voxelost@proton.me>
2023-11-18 00:10:21 +0100

    poc edits

--- homelab/vm-manager/src/main.py
+++ homelab/vm-manager/src/main.py
@@ -4,35 +4,36 @@ from utils.conn import conn_wrapper
 from wrappers.domain import Domain
 from models.libvirt.snapshot import DomainSnapshot, Name, Description
 from utils.dump import setup_logging, destroy_all_vms
-
+from io import StringIO
 
 if __name__ == "__main__":
     setup_logging()
 
-    with conn_wrapper() as conn:
-        if len(conn.listAllDomains()) > 5:
-            destroy_all_vms(conn)
+    conn = conn_wrapper()
+    if len(conn.listAllDomains()) > 5:
+        destroy_all_vms(conn)
 
-        new_dom = Domain.create_default(conn)
-        res = new_dom.exec_ssh_cmd('cloud-init status --wait')
+    new_dom = Domain.create_default(conn)
+    res = new_dom.exec_ssh_cmd("cloud-init status --wait")
 
-        logging.debug(f'cloud init status: {res}')
-        logging.debug(f'domain time: {new_dom.getTime()}')
+    logging.debug(f"cloud init status: {res}")
+    logging.debug(f"domain time: {new_dom.getTime()}")
 
-        preupload_snapshot = new_dom.create_snapshot()
+    logging.debug("Creating a snapshot")
+    preupload_snapshot = new_dom.create_snapshot()
 
-        from io import StringIO
-        with StringIO('hello from controller') as fptr:
-            new_dom.upload_file(fptr, '/home/kim/hello')
+    with StringIO("hello from controller") as fptr:
+        new_dom.upload_file(fptr, "/home/kim/hello")
 
-        res = new_dom.exec_ssh_cmd('cat /home/kim/hello')
-        logging.debug(res)
+    res = new_dom.exec_ssh_cmd("cat /home/kim/hello")
+    logging.debug(res)
 
-        snapshot_fetched = new_dom.snapshotCurrent()
-        assert preupload_snapshot.getXMLDesc() == snapshot_fetched.getXMLDesc()
+    snapshot_fetched = new_dom.snapshotCurrent()
+    assert preupload_snapshot.getXMLDesc() == snapshot_fetched.getXMLDesc()
 
-        new_dom.revertToSnapshot(snapshot_fetched)
-        try:
-            new_dom.exec_ssh_cmd('cat /home/kim/hello')
-        except Exception as e:
-            logging.error(e)
+    logging.debug("Reverting to the snapshot from before file upload")
+    new_dom.revertToSnapshot(snapshot_fetched)
+    try:
+        new_dom.exec_ssh_cmd("cat /home/kim/hello")
+    except Exception as e:
+        logging.error(e)
