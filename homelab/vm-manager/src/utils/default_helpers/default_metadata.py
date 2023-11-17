55ddf12394d3bb7408b91331665dd4f8196330c1
Emilia <voxelost@proton.me>
2023-11-18 00:05:19 +0100

    move default helper functions to classmethods

--- homelab/vm-manager/src/utils/default_helpers/default_metadata.py
+++
@@ -1,7 +0,0 @@
-from models.cloud_init.metadata import MetaData
-
-def get_default_metadata() -> MetaData:
-    return MetaData(
-        instance_id='debby',
-        local_hostname='debby',
-    )
