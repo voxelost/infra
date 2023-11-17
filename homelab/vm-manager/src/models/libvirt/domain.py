2ff13d418f11eaa345974415dd54bea44d657b77
Emilia <voxelost@proton.me>
2023-11-18 00:05:37 +0100

    reformat code

--- homelab/vm-manager/src/models/libvirt/domain.py
+++ homelab/vm-manager/src/models/libvirt/domain.py
@@ -5,12 +5,14 @@ from typing import List, Optional, Union, IO
 from os import PathLike
 
 LIBOS_METADATA_NS = "http://libosinfo.org/xmlns/libvirt/domain/1.0"
+
+
 @xml_dataclass
 @dataclass
 class Address:
     __ns__ = None
 
-    type_value: Optional[str] = rename(field(default=None), name='type')
+    type: Optional[str] = field(default=None)
     domain: Optional[str] = field(default=None)
     bus: Optional[str] = field(default=None)
     slot: Optional[str] = field(default=None)
@@ -21,6 +23,7 @@ class Address:
     port: Optional[str] = field(default=None)
     multifunction: Optional[str] = field(default=None)
 
+
 @xml_dataclass
 @dataclass
 class Alias:
@@ -28,13 +31,15 @@ class Alias:
 
     name: Optional[str] = field(default=None)
 
+
 @xml_dataclass
 @dataclass
 class Audio:
     __ns__ = None
 
     id: Optional[str] = field(default=None)
-    type_value: Optional[str] = rename(field(default=None), name='type')
+    type: Optional[str] = field(default=None)
+
 
 @xml_dataclass
 @dataclass
@@ -42,9 +47,8 @@ class Backend:
     __ns__ = None
 
     model: Optional[str] = field(default=None)
-    value: str = text(field(
-        default="",
-    ))
+    value: str = text(field(default="/dev/urandom"))
+
 
 @xml_dataclass
 @dataclass
@@ -52,6 +56,7 @@ class Boot:
     __ns__ = None
 
     dev: Optional[str] = field(default=None)
+
 
 @xml_dataclass
 @dataclass
@@ -62,13 +67,15 @@ class Cpu:
     check: Optional[str] = field(default=None)
     migratable: Optional[str] = field(default=None)
 
+
 @xml_dataclass
 @dataclass
 class Driver:
     __ns__ = None
 
     name: Optional[str] = field(default=None)
-    type_value: Optional[str] = rename(field(default=None), name='type')
+    type: Optional[str] = field(default=None)
+
 
 @xml_dataclass
 @dataclass
@@ -77,13 +84,15 @@ class Image:
 
     compression: Optional[str] = field(default=None)
 
+
 @xml_dataclass
 @dataclass
 class Listen:
     __ns__ = None
 
-    type_value: Optional[str] = rename(field(default=None), name='type')
+    type: Optional[str] = field(default=None)
     address: Optional[str] = field(default=None)
+
 
 @xml_dataclass
 @dataclass
@@ -91,6 +100,7 @@ class Mac:
     __ns__ = None
 
     address: Optional[str] = field(default=None)
+
 
 @xml_dataclass
 @dataclass
@@ -100,6 +110,7 @@ class Memory:
     unit: Optional[str] = field(default=None)
     value: Optional[str] = text(field(default=None))
 
+
 @xml_dataclass
 @dataclass
 class Uuid:
@@ -107,15 +118,17 @@ class Uuid:
 
     value: Optional[str] = text(field(default=None))
 
+
 @xml_dataclass
 @dataclass
 class Model:
     __ns__ = None
 
-    type_value: Optional[str] = rename(field(default=None), name='type')
+    type: Optional[str] = field(default=None)
     heads: Optional[str] = field(default=None)
     primary: Optional[str] = field(default=None)
     name: Optional[str] = field(default=None)
+
 
 @xml_dataclass
 @dataclass
@@ -124,6 +137,7 @@ class ResourcePartition:
 
     partition: Optional[str] = text(field(default=None))
 
+
 @xml_dataclass
 @dataclass
 class Resource:
@@ -131,28 +145,35 @@ class Resource:
 
     partition: Optional[ResourcePartition] = field(default=None)
 
+
 @xml_dataclass
 @dataclass
 class SeclabelLabel:
     __ns__ = None
 
     value: Optional[str] = text(field(default=None))
+
+
 @xml_dataclass
 @dataclass
 class SeclabelImageLabel:
     __ns__ = None
 
     value: Optional[str] = text(field(default=None))
+
+
 @xml_dataclass
 @dataclass
 class Seclabel:
     __ns__ = None
 
-    type_value: Optional[str] = rename(field(default=None), name='type')
+    type: Optional[str] = field(default=None)
     model: Optional[str] = field(default=None)
     relabel: Optional[str] = field(default=None)
     label: Optional[SeclabelLabel] = field(default=None)
     imagelabel: Optional[SeclabelImageLabel] = field(default=None)
+
+
 @xml_dataclass
 @dataclass
 class Source:
@@ -166,6 +187,7 @@ class Source:
     path: Optional[str] = field(default=None)
     mode: Optional[str] = field(default=None)
 
+
 @xml_dataclass
 @dataclass
 class SuspendToDisk:
@@ -173,12 +195,14 @@ class SuspendToDisk:
 
     enabled: Optional[str] = field(default=None)
 
+
 @xml_dataclass
 @dataclass
 class SuspendToMem:
     __ns__ = None
 
     enabled: Optional[str] = field(default=None)
+
 
 @xml_dataclass
 @dataclass
@@ -189,6 +213,7 @@ class Timer:
     present: Optional[str] = field(default=None)
     tickpolicy: Optional[str] = field(default=None)
 
+
 @xml_dataclass
 @dataclass
 class TypeType:
@@ -196,9 +221,12 @@ class TypeType:
 
     arch: Optional[str] = field(default=None)
     machine: Optional[str] = field(default=None)
-    value: str = text(field(
-        default="",
-    ))
+    value: str = text(
+        field(
+            default="",
+        )
+    )
+
 
 @xml_dataclass
 @dataclass
@@ -208,12 +236,14 @@ class Vcpu:
     placement: Optional[str] = field(default=None)
     value: Optional[str] = text(field(default=None))
 
+
 @xml_dataclass
 @dataclass
 class Vmport:
     __ns__ = None
 
     state: Optional[str] = field(default=None)
+
 
 @xml_dataclass
 @dataclass
@@ -222,25 +252,32 @@ class Os2:
 
     id: Optional[str] = field(default=None)
 
+
 @xml_dataclass
 @dataclass
 class Clock:
     __ns__ = None
 
     offset: Optional[str] = field(default=None)
-    timer: List[Timer] = field(default_factory=list)
+    timers: List[Timer] = rename(field(default_factory=list), name="timer")
+
+
 @xml_dataclass
 @dataclass
 class Acpi:
     __ns__ = None
 
     value: Optional[str] = text(field(default=None))
+
+
 @xml_dataclass
 @dataclass
 class Apic:
     __ns__ = None
 
     value: Optional[str] = text(field(default=None))
+
+
 @xml_dataclass
 @dataclass
 class Features:
@@ -250,17 +287,19 @@ class Features:
     apic: Optional[Apic] = field(default=None)
     vmport: Optional[Vmport] = field(default=None)
 
+
 @xml_dataclass
 @dataclass
 class Graphics:
     __ns__ = None
 
-    type_value: Optional[str] = rename(field(default=None), name='type')
+    type: Optional[str] = field(default=None)
     port: Optional[str] = field(default=None)
     autoport: Optional[str] = field(default=None)
-    listen_attribute: Optional[str] = rename(field(default=None), name='listen')
+    listen_attribute: Optional[str] = rename(field(default=None), name="listen")
     listen: Optional[Listen] = field(default=None)
     image: Optional[Image] = field(default=None)
+
 
 @xml_dataclass
 @dataclass
@@ -271,12 +310,13 @@ class Memballoon:
     alias: Optional[Alias] = field(default=None)
     address: Optional[Address] = field(default=None)
 
+
 @xml_dataclass
 @dataclass
 class Os1:
     __ns__ = None
 
-    type_value: Optional[TypeType] = rename(field(default=None), name='type')
+    type: Optional[TypeType] = field(default=None)
     boot: Optional[Boot] = field(default=None)
 
 
@@ -285,8 +325,13 @@ class Os1:
 class Pm:
     __ns__ = None
 
-    suspend_to_mem: Optional[SuspendToMem] = rename(field(default=None), name='suspend-to-mem')
-    suspend_to_disk: Optional[SuspendToDisk] = rename(field(default=None), name='suspend-to-disk')
+    suspend_to_mem: Optional[SuspendToMem] = rename(
+        field(default=None), name="suspend-to-mem"
+    )
+    suspend_to_disk: Optional[SuspendToDisk] = rename(
+        field(default=None), name="suspend-to-disk"
+    )
+
 
 @xml_dataclass
 @dataclass
@@ -298,6 +343,7 @@ class Rng:
     alias: Optional[Alias] = field(default=None)
     address: Optional[Address] = field(default=None)
 
+
 @xml_dataclass
 @dataclass
 class Sound:
@@ -307,12 +353,13 @@ class Sound:
     alias: Optional[Alias] = field(default=None)
     address: Optional[Address] = field(default=None)
 
+
 @xml_dataclass
 @dataclass
 class Target:
     __ns__ = None
 
-    type_value: Optional[str] = rename(field(default=None), name='type')
+    type: Optional[str] = field(default=None)
     port: Optional[str] = field(default=None)
     model: Optional[Model] = field(default=None)
     dev: Optional[str] = field(default=None)
@@ -320,6 +367,7 @@ class Target:
     name: Optional[str] = field(default=None)
     state: Optional[str] = field(default=None)
     chassis: Optional[str] = field(default=None)
+
 
 @xml_dataclass
 @dataclass
@@ -330,22 +378,33 @@ class Video:
     alias: Optional[Alias] = field(default=None)
     address: Optional[Address] = field(default=None)
 
+
 @xml_dataclass
 @dataclass
 class Libosinfo:
     __ns__ = LIBOS_METADATA_NS
-    __nsmap__ = {
-        'libosinfo': LIBOS_METADATA_NS
-    }
+    __nsmap__ = {"libosinfo": LIBOS_METADATA_NS}
 
     os: Optional[Os2] = field(default=None)
+
+
+@xml_dataclass
+@dataclass
+class VmManagerMetadata:
+    __ns__ = "https://internal.local"
+    __nsmap__ = {
+        "internal": "https://internal.local",
+    }
+
+    data: Optional[str] = text(field(default=None))
+
 
 @xml_dataclass
 @dataclass
 class Console:
     __ns__ = None
 
-    type_value: Optional[str] = rename(field(default=None), name='type')
+    type: Optional[str] = field(default=None)
     tty: Optional[str] = field(default=None)
     source: Optional[Source] = field(default=None)
     target: Optional[Target] = field(default=None)
@@ -373,22 +432,25 @@ class ReadOnly:
 class Disk:
     __ns__ = None
 
-    type_value: Optional[str] = rename(field(default=None), name='type')
+    type: Optional[str] = field(default=None)
     device: Optional[str] = field(default=None)
     driver: Optional[Driver] = field(default=None)
     source: Optional[Source] = field(default=None)
-    backing_store: Optional[BackingStore] = rename(field(default=None), name='backingStore')
+    backing_store: Optional[BackingStore] = rename(
+        field(default=None), name="backingStore"
+    )
     readonly: Optional[ReadOnly] = field(default=None)
     target: Optional[Target] = field(default=None)
     alias: Optional[Alias] = field(default=None)
     address: Optional[Address] = field(default=None)
+
 
 @xml_dataclass
 @dataclass
 class Interface:
     __ns__ = None
 
-    type_value: Optional[str] = rename(field(default=None), name='type')
+    type: Optional[str] = field(default=None)
     mac: Optional[Mac] = field(default=None)
     source: Optional[Source] = field(default=None)
     target: Optional[Target] = field(default=None)
@@ -396,24 +458,26 @@ class Interface:
     alias: Optional[Alias] = field(default=None)
     address: Optional[Address] = field(default=None)
 
+
 @xml_dataclass
 @dataclass
 class Metadata:
     __ns__ = None
 
     libosinfo: Optional[Libosinfo] = field(default=None)
+    vm_manager: Optional[VmManagerMetadata] = field(default=None)
 
-    # TODO: add application specific metadata
 
 @xml_dataclass
 @dataclass
 class Serial:
     __ns__ = None
 
-    type_value: Optional[str] = rename(field(default=None), name='type')
+    type: Optional[str] = field(default=None)
     source: Optional[Source] = field(default=None)
     target: Optional[Target] = field(default=None)
     alias: Optional[Alias] = field(default=None)
+
 
 @xml_dataclass
 @dataclass
@@ -422,28 +486,30 @@ class Emulator:
 
     value: Optional[str] = text(field(default=None))
 
+
 @xml_dataclass
 @dataclass
 class Channel:
     __ns__ = None
 
-    type_value: Optional[str] = rename(field(default=None), name='type')
+    type: Optional[str] = field(default=None)
     target: Optional[Target] = field(default=None)
     alias: Optional[Alias] = field(default=None)
     address: Optional[Address] = field(default=None)
     source: Optional[Source] = field(default=None)
+
 
 @xml_dataclass
 @dataclass
 class Controller:
     __ns__ = None
 
-    type_value: Optional[str] = rename(field(default=None), name='type')
+    type: Optional[str] = field(default=None)
     index: Optional[str] = field(default=None)
     model: Optional[str] = field(default=None)
     alias: Optional[Alias] = field(default=None)
     address: Optional[Address] = field(default=None)
-    model_attribute: Optional[str] = rename(field(default=None), name='model')
+    model_attribute: Optional[str] = rename(field(default=None), name="model")
     model: Optional[Model] = field(default=None)
     target: Optional[Target] = field(default=None)
 
@@ -453,7 +519,7 @@ class Controller:
 class Input:
     __ns__ = None
 
-    type_value: Optional[str] = rename(field(default=None), name='type')
+    type: Optional[str] = field(default=None)
     bus: Optional[str] = field(default=None)
     alias: Optional[Alias] = field(default=None)
 
@@ -464,7 +530,7 @@ class Devices:
     __ns__ = None
 
     emulator: Optional[Emulator] = field(default=None)
-    disks: List[Disk] = rename(field(default_factory=list), name='disk')
+    disks: List[Disk] = rename(field(default_factory=list), name="disk")
     interface: Optional[Interface] = field(default=None)
     serial: Optional[Serial] = field(default=None)
     console: Optional[Console] = field(default=None)
@@ -475,8 +541,10 @@ class Devices:
     memballoon: Optional[Memballoon] = field(default=None)
     rng: Optional[Rng] = field(default=None)
     channel: Optional[Channel] = field(default=None)
-    inputs: List[Input] = rename(field(default_factory=list), name='input')
-    controllers: List[Controller] = rename(field(default_factory=list), name='controller')
+    inputs: List[Input] = rename(field(default_factory=list), name="input")
+    controllers: List[Controller] = rename(
+        field(default_factory=list), name="controller"
+    )
 
 
 @xml_dataclass
@@ -486,12 +554,14 @@ class OnPoweroff:
 
     value: Optional[str] = text(field(default=None))
 
+
 @xml_dataclass
 @dataclass
 class OnReboot:
     __ns__ = None
 
     value: Optional[str] = text(field(default=None))
+
 
 @xml_dataclass
 @dataclass
@@ -500,6 +570,7 @@ class OnCrash:
 
     value: Optional[str] = text(field(default=None))
 
+
 @xml_dataclass
 @dataclass
 class DomainName:
@@ -507,12 +578,13 @@ class DomainName:
 
     value: Optional[str] = text(field(default=None))
 
+
 @xml_dataclass
 @dataclass
 class LibvirtDomain(XmlDataclass):
     __ns__ = None
 
-    type_value: Optional[str] = rename(field(default=None), name='type')
+    type: Optional[str] = field(default=None)
     id: Optional[str] = field(default=None)
     name: Optional[DomainName] = field(default=None)
     metadata: Optional[Metadata] = field(default=None)
@@ -528,25 +600,23 @@ class LibvirtDomain(XmlDataclass):
     on_crash: Optional[OnCrash] = field(default=None)
     pm: Optional[Pm] = field(default=None)
     devices: Optional[Devices] = field(default=None)
-    seclabel: List[Seclabel] = field(default_factory=list)
+    seclabels: List[Seclabel] = rename(field(default_factory=list), name="seclabel")
     uuid: Optional[Uuid] = field(default=None)
-    current_memory: Optional[Memory] = rename(field(default=None), name='currentMemory')
+    current_memory: Optional[Memory] = rename(field(default=None), name="currentMemory")
 
     @classmethod
     def from_xml_string(cls, source: str):
         parser = etree.XMLParser(remove_blank_text=True, remove_comments=True)
         lxml_el_in = etree.fromstring(source, parser).getroot()
-        return load(LibvirtDomain, lxml_el_in, 'domain')
+        return load(LibvirtDomain, lxml_el_in, "domain")
 
     @classmethod
     def from_xml_file(cls, source: Union[PathLike, IO]):
         parser = etree.XMLParser(remove_blank_text=True, remove_comments=True)
         lxml_el_in = etree.parse(source, parser).getroot()
-        return load(LibvirtDomain, lxml_el_in, 'domain')
+        return load(LibvirtDomain, lxml_el_in, "domain")
 
     def to_xml_string(self, indent=False) -> str:
         return etree.tostring(
-            dump(self, "domain", {}),
-            encoding="unicode",
-            pretty_print=indent
+            dump(self, "domain", {}), encoding="unicode", pretty_print=indent
         )
