6cd45ebbeb0109dc3f76157a431ce9b7157999a1
Emilia <voxelost@proton.me>
2023-11-18 11:12:13 +0100

    add userdata password and authd ssh key setters

--- homelab/vm-manager/src/models/cloud_init/userdata.py
+++ homelab/vm-manager/src/models/cloud_init/userdata.py
@@ -1,6 +1,9 @@
 from dataclasses import dataclass, field, asdict
 from typing import List, Optional
 from models.cloud_init.cloud_init import CloudInitObj
+from utils.ssh import get_nuc_pkey
+from passlib.hash import md5_crypt
+from paramiko import PKey
 
 import yaml
 
@@ -15,10 +18,21 @@ class User:
     groups: Optional[str] = field(default=None)
     shell: str = field(default="/bin/bash")
 
+    @property
+    def password(self): ...
+
+    @password.setter
+    def password(self, new_pass: str):
+        self.hashed_passwd = md5_crypt.hash(new_pass)
+
+    def add_authorized_key(self, pem_key: PKey):
+        if not self.ssh_authorized_keys:
+            self.ssh_authorized_keys = []
+        self.ssh_authorized_keys.append(f'{pem_key.name} {pem_key.get_base64()}')
 
 @dataclass
 class UserData(CloudInitObj):
-    _FILENAME = 'meta-data'
+    _FILENAME = 'user-data'
 
     users: List[User] = field(default_factory=list)
 
@@ -29,22 +43,25 @@ class UserData(CloudInitObj):
     runcmd: Optional[List[str]] = field(default=None)
 
     @classmethod
-    def create_default(cls):
+    def create_default(cls, pem_key: Optional[PKey] = None):
+        default_user = User(
+            name="kim",
+            lock_passwd=False,
+            sudo=["ALL=(ALL) NOPASSWD:ALL"],
+            groups="users, sudo",
+            shell="/bin/bash",
+        )
+
+        default_user.password = 'possible'
+
+        if pem_key:
+            default_user.add_authorized_key(pem_key)
+        else:
+            default_user.add_authorized_key(get_nuc_pkey())
+
         return cls(
-            users=[
-                User(
-                    name="kim",
-                    lock_passwd=False,
-                    hashed_passwd="$1$x9fXkSPH$p1ffAhqDKAvT4Stz35l4C/",  # passwd: 'possible' # <output from openssl passwd -1>
-                    ssh_authorized_keys=[
-                        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAD9QD3Ktvnq5P9oa4XxZ/h1tSAQnGdZkjAxCU0jlZ1UsWp9hf0/vRLlvHa6B6xaaaZZfaIuUkh3Y+y6obRbul+SZ1ru8ixj+KJkl0b0ZL20PJ0lbN3n8IiE1x53xCsXkjt0F7GHVmzUMuyguff0bnCd2Nf6O0u6ja0766NwJ5G565lBkZT7Q4u38+XZSFH4r9F9emWZxEF9EfteF8LqyLlCDAcrPDbe6iqhXqjUlTD6uNORMKh1i1U49yB6khNcy2rwJYuIFtd0KSqShhHBkWGdrsUnjN7KEEuc0ORvfryJDaEcIoqodXcYbHH2W0gE4DHl71n4l2/XKg5z1NzUWcxcJWSnmfwTkynVFqpdAm0hz+54andsya9exHbgWkqsoaZx/ecZNUySoIgnQGXp+yGUg6q0DwVDA4EdWhlmwSpJ+EU6tozzaxsRK7mZZTpC4sz/wnJNb5itjAS2vd3wxslPRliIyQFvNrNc5bGY9G4ECA6+CbVh8KexSNZD08udypiVO875TZM0WUbN+TmTtjfMAcdfYji0ZNRMv02nnMkll754KKe9BplzAjNYKy7ol3rb9/shA/aoihEi7pLhUEl6jjdT/el43MBzxSe5uJpCs/i3KYj8AcxKVqmcN1P9M5JOFqMNcT5lG3slMlfXGhZ6ZTPpSVFY2XF8GiXAvPMCfRxsZWpf3wlBNv+GKMlOOE098QKr7m97YIXsBCpDzrneZQTI5lI9tHrFDEU0I9A7qKjrnXV+j4JLXyLJ3hMUnyAgKNbNeV2rnQrRvw/3m9Svsmrn/ozWMdvc9uLUZHXojiQbGPwhy2Ycw0lheIv0F25sQSqS0UicVf63wNhy3NC7u11Pm2p9HO2qKpe2xvG5E/VS2v6Z8XErm4D3HWdoA20d4ljOU1UNTnP1HW04Xs0QjVs9CniX/1zdsNIHWV+wE74tjDVPyyF/kh4ptbf/Rt7FPksZe3eyd/YLGHf/lejo59zL+HRRkjsaA1CYzpg9NSzeSIX/MROwsV2pcRtboCnrX8L+PpbkDOor/ku96x2qj52gKi32atuKeYja+m6+3rDPbgl6XWTCUCcxfB9FZ/5JpHeCOFqKQE1O3KKODT3wrEaeWfWXWoawynwLnd/020i7oEUOBTX6z+ViONBzLkM71Boa3iVLToB/6AjbmFd3tv7KLxAqutP8emI2efrn0eDh3R0bZJ8oCy01d6jFEllIFYI/t7e8u/iPWbFKx8Shn7whgH0BFR8+KbvfCxLlmg7v4lpCEr7ucE9YZDydXaDGN37G3W5Gn3yt/0LApIh11gr+Zp82jIgdmGfhOErSop7/c/b0mndk4XCK2SICYbtLZ74FnRV",
-                    ],
-                    sudo=["ALL=(ALL) NOPASSWD:ALL"],
-                    groups="users, sudo",
-                    shell="/bin/bash",
-                )
-            ],
-            # package_update=True, # TODO: temporarily disabled because it took over 5 minutes
+            users=[default_user],
+            package_update=True,
             package_upgrade=True,
             packages=["qemu-guest-agent"],
             runcmd=["systemctl enable --now qemu-guest-agent.service"],
