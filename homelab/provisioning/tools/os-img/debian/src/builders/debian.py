00158932ea4699fb56866343515c354179475d9c
Emilia <voxelost@proton.me>
2023-11-20 01:39:44 +0100

    add firstboot script logic

--- homelab/provisioning/tools/os-img/debian/src/builders/debian.py
+++ homelab/provisioning/tools/os-img/debian/src/builders/debian.py
@@ -12,6 +12,10 @@ def build_iso(
     **_
 ):
     with Multipass(target_config_file, multipass_auth) as multipass:
+        """
+        https://stackoverflow.com/questions/59940351/how-to-mount-a-memory-filesystem-onto-a-directory
+
+        """
         multipass.cmd("apt-get update")
         multipass.cmd("apt-get upgrade -y")
         multipass.cmd("apt-get -y install genisoimage xorriso isolinux udevil")
@@ -28,15 +32,19 @@ def build_iso(
         multipass.cmd("chmod +w -R isofiles/install.amd/")
         multipass.cmd("gunzip isofiles/install.amd/initrd.gz")
 
-        multipass.upload_rendered_template("preseed.cfg.j2")
-        multipass.upload_rendered_template("setup.sh.j2")
+        _template_files = [
+            "preseed.cfg",
+            "preseed-setup.sh",
+            "firstboot-setup.sh",
+            "firstboot.service",
+        ]
 
-        multipass.cmd(
-            "cpio -H newc -o -A -F isofiles/install.amd/initrd", stdin="preseed.cfg"
-        )
-        multipass.cmd(
-            "cpio -H newc -o -A -F isofiles/install.amd/initrd", stdin="setup.sh"
-        )
+        for fname in _template_files:
+            multipass.upload_rendered_template(f"{fname}.j2")
+            multipass.cmd(
+                "cpio -H newc -o -A -F isofiles/install.amd/initrd", stdin=fname
+            )
+
         multipass.cmd("gzip isofiles/install.amd/initrd")
         multipass.cmd("chmod -w -R isofiles/install.amd/")
         multipass.cmd("chmod +w md5sum.txt", cwd="isofiles")
@@ -46,7 +54,15 @@ def build_iso(
         )
         multipass.cmd("chmod -w md5sum.txt", cwd="isofiles")
         multipass.cmd(
-            f"xorriso -as mkisofs -o {target_iso_filename} -isohybrid-mbr /usr/lib/ISOLINUX/isohdpfx.bin -c isolinux/boot.cat -b isolinux/isolinux.bin -no-emul-boot -boot-load-size 4 -boot-info-table isofiles",
+            "xorriso -as mkisofs "
+            f"-o {target_iso_filename} "
+            "-isohybrid-mbr /usr/lib/ISOLINUX/isohdpfx.bin "
+            "-c isolinux/boot.cat "
+            "-b isolinux/isolinux.bin "
+            "-no-emul-boot "
+            "-boot-load-size 4 "
+            "-boot-info-table "
+            "isofiles"
         )
         multipass.cmd("chmod +w -R isofiles")
         multipass.cmd("rm -r isofiles")
