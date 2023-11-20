04525b5c0bc8ab8ed523c45bbeac9f67a68ba751
Emilia <voxelost@proton.me>
2023-11-20 01:40:06 +0100

    refactor config setup for recursive updates

--- homelab/provisioning/tools/os-img/debian/src/utils/config.py
+++ homelab/provisioning/tools/os-img/debian/src/utils/config.py
@@ -1,6 +1,7 @@
 import os
 from functools import cached_property
 import yaml
+import collections.abc
 
 
 class Config(dict):
@@ -12,11 +13,23 @@ class Config(dict):
     ):
         if os.path.exists(default_config_file):
             with open(default_config_file, "r") as fptr:
-                self.update(yaml.safe_load(fptr))
+                # update(self, yaml.safe_load(fptr))
+                _update(self, yaml.safe_load(fptr))
 
         if os.path.exists(commons_config_file):
             with open(commons_config_file, "r") as fptr:
-                self.update(yaml.safe_load(fptr))
+                # update(self, yaml.safe_load(fptr))
+                _update(self, yaml.safe_load(fptr))
 
         with open(config_file, "r") as fptr:
-            self.update(yaml.safe_load(fptr))
+            # update(self, yaml.safe_load(fptr))
+            _update(self, yaml.safe_load(fptr))
+
+
+def _update(self, other):
+    for k, v in other.items():
+        if isinstance(v, collections.abc.Mapping):
+            self[k] = _update(self.get(k, {}), v)
+        else:
+            self[k] = v
+    return self
